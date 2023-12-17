94786d1ca1e8fe1de658fdc55b86c42234c2c747
lolito-lindsey-official <lolitolindsey@gmail.com>
2023-12-17 13:15:19 -0600

    Update script.js
    Added a digital clock to the home page.

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ window.addEventListener('resize', () => {
     desktopNav.style.display = 'none';
   }
 });
+
+// Set up the the function for the clock
+function cycleClock() {
+  // Call for current time
+  const nowTime = new Date();
+  // Set up the hours, minutes, and seconds
+  const hourTime = String(nowTime.getHours()).padStart(2, '0');
+  const minuteTime = String(nowTime.getMinutes()).padStart(2, '0');
+  const secondTime =String(nowTime.getSeconds()).padStart(2, '0');
+  // Make a string to display the HH:MM:SS model
+  const timeString = `The current time is:  ${hourTime}:${minuteTime}:${secondTime}`;
+  // Bring the 'todo-clock' id here to display the time
+  document.getElementById('display-clock').innerText = timeString;
+}
+
+// 1000ms = 1 second
+setInterval(cycleClock, 1000);
+cycleClock();
